8746f0c75c58c2f6ca105b310e5985faff82cb6a
Adam Soltan <adam.henryk.soltan@gmail.com>
2023-10-28 13:40:16 +0200

    Cleanup centroid tree

--- graph/ctree.cpp
+++ graph/ctree.cpp
@@ -3,7 +3,7 @@ struct ctree {
   vector<vector<int>> adj;
   vector<int> sz, up;
   vector<bool> vis;
-
+ 
   ctree(int _n) {
     n = _n;
     adj.resize(n);
@@ -11,13 +11,13 @@ struct ctree {
     up.resize(n, -1);
     vis.resize(n);
   }
-
+ 
   void add_edge(int u, int v) {
     adj[u].push_back(v);
     adj[v].push_back(u);
   }
-
-  int dfs(int u, int p) {
+ 
+  void dfs(int u, int p) {
     sz[u] = 1;
     for (int v : adj[u]) {
       if (!vis[v] && v != p) {
@@ -27,23 +27,26 @@ struct ctree {
     }
   }
   
-  int search(int u, int p, int s) {
-    for (int v : adj[u]) {
-      if (!vis[v] && v != p && sz[v] > s / 2) {
-        return search(v, u, s);
-      }
-    }
-    return u;
-  }
-
   int build(int u = 0) {
     dfs(u, -1);
-    int c = search(u, -1, sz[r]);
+    int c = u;
+    bool ok = false;
+    while (!ok) {
+      ok = true;
+      for (int v : adj[c]) {
+        if (!vis[v] && sz[v] < sz[c] && sz[v] > sz[u] / 2) {
+          c = v;
+          ok = false;
+          break;
+        }
+      }
+    }
     vis[c] = true;
     for (int v : adj[c]) {
       if (!vis[v]) {
         up[build(v)] = c;
       }
     }
+    return c;
   }
 };
