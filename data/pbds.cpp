0aabeb1e0301d8fffefa8f9d793792d826c44cfd
Adam Soltan <adam.henryk.soltan@gmail.com>
2024-02-05 14:58:28 +0100

    Remove gp_hash_table

--- data/pbds.cpp
+++ data/pbds.cpp
@@ -2,22 +2,5 @@
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 
-struct chash {
-  static uint64_t splitmix64(uint64_t x) {
-    x += 0x9e3779b97f4a7c15;
-    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-    return x ^ (x >> 31);
-  }
-
-  size_t operator()(uint64_t x) const {
-    static const uint64_t add = chrono::steady_clock::now().time_since_epoch().count();
-    return splitmix64(x + add);
-  }
-};
-
-template<typename A, typename B>
-using hash_map = gp_hash_table<A, B, chash>;
-
 template<typename T>
 using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
