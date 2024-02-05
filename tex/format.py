9da8b43d3e67a13ad917100093f2664ed157255f
Adam Soltan <adam.henryk.soltan@gmail.com>
2024-02-05 20:13:15 +0100

    Fix broken pipe error

--- tex/format.py
+++ tex/format.py
@@ -1,7 +1,9 @@
 import subprocess, re, sys
 
+assert(len(sys.argv) == 4)
 path = sys.argv[1]
 lang = sys.argv[2]
+out = sys.argv[3]
 
 if lang == "C++":
   src = subprocess.run(["clang-format", path], stdout=subprocess.PIPE).stdout.decode()
@@ -10,4 +12,5 @@ else:
     src = f.read()
 src = re.sub(r'(for|while|if|else|else if)(.*){ (.*;)\s*}', r'\1\2\3', src)
 
-print(f"\\begin{{lstlisting}}[language={lang}]\n{src.rstrip()}\n\\end{{lstlisting}}")
+with open(out, "w") as f:
+  f.write(f"\\begin{{lstlisting}}[language={lang}]\n{src.rstrip()}\n\\end{{lstlisting}}")
