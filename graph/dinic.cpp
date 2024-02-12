f2eb5828ffdff7502d697b27ff983b3b2f17ea1e
Adam Soltan <adam.henryk.soltan@gmail.com>
2024-02-12 21:08:35 +0100

    Cleanup Dinic

--- graph/dinic.cpp
+++ graph/dinic.cpp
@@ -6,37 +6,34 @@ struct dinic {
 
   int n;
   vector<vector<edge>> adj;
-  vector<int> lvl, it;
+  vector<int> q, lvl, it;
 
   dinic(int _n) {
     n = _n;
     adj.resize(n);
+    q.reserve(n);
     lvl.resize(n);
     it.resize(n);
   }
 
   void add_edge(int u, int v, int cap) {
     int i = ssize(adj[u]);
-    int j = ssize(adj[v]);
-    if (u == v) {
-      j++;
-    }
+    int j = ssize(adj[v]) + (u == v);
     adj[u].push_back({v, j, cap});
     adj[v].push_back({u, i, 0});
   }
 
   bool bfs(int s, int t) {
+    q.clear();
     lvl.assign(n, -1);
-    queue<int> q;
     lvl[s] = 0;
-    q.push(s);
-    while (!q.empty()) {
-      int u = q.front();
-      q.pop();
+    q.push_back(s);
+    for (int i = 0; i < ssize(q); i++) {
+      int u = q[i];
       for (edge& e : adj[u]) {
         if (e.cap > 0 && lvl[e.to] == -1) {
           lvl[e.to] = lvl[u] + 1;
-          q.push(e.to);
+          q.push_back(e.to);
           if (e.to == t) {
             return true;
           }
@@ -46,33 +43,33 @@ struct dinic {
     return false;
   }
 
-  int dfs(int u, int t, int cap) {
+  ll dfs(int u, int t, ll cap) {
     if (u == t) {
       return cap;
     }
-    int ans = 0;
+    ll f = 0;
     for (int& i = it[u]; i < ssize(adj[u]); i++) {
       edge& e = adj[u][i];
       if (e.cap > 0 && lvl[u] + 1 == lvl[e.to]) {
-        int add = dfs(e.to, t, min(cap - ans, e.cap));
+        ll add = dfs(e.to, t, min(cap - f, (ll)e.cap));
         e.cap -= add;
         adj[e.to][e.rev].cap += add;
-        ans += add;
+        f += add;
       }
-      if (ans == cap) {
-        return ans;
+      if (f == cap) {
+        return f;
       }
     }
     lvl[u] = -1;
-    return ans;
+    return f;
   }
 
-  int flow(int s, int t, int cap) {
-    int ans = 0;
-    while (ans < cap && bfs(s, t)) {
+  ll flow(int s, int t, ll cap) {
+    ll f = 0;
+    while (f < cap && bfs(s, t)) {
       it.assign(n, 0);
-      ans += dfs(s, t, cap - ans);
+      f += dfs(s, t, cap - f);
     }
-    return ans;
+    return f;
   }
 };
